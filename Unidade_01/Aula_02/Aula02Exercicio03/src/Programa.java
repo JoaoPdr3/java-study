96d3d6e0e0177866b1e14167d0d5697ceaebdefd
João Moreira <98332927+Joao-M0@users.noreply.github.com>
2022-05-18 19:26:08 -0300

    Add files via upload

---
+++ Unidade_01/Aula_02/Aula02Exercicio03/src/Programa.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Programa {
+	@SuppressWarnings("all")
+	public static void main(String[] args) {
+		/* Leia a idade e a altura de 10 pessoas. 
+		 * Calcule e informe a média das alturas 
+		 * das pessoas com mais de 50 anos.*/
+		Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+		
+		int pessoas = 0;
+		int idade = 0;
+		double altura;
+		double somaAltura = 0;
+		double media = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Digite sua idade: ");
+			idade = entrada.nextInt();
+			System.out.println("Digite sua altura: ");
+			altura = entrada.nextDouble();
+			
+			if(idade >= 50) {
+				somaAltura += altura;
+				pessoas += 1;
+			}
+			media = somaAltura/pessoas;
+		}
+		System.out.printf("A média é: %.2f ",media);
+	}
+}
---
+++ Unidade_01/Aula_02/Aula02Exercicio04/src/Programa.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Programa {
+	@SuppressWarnings("all")
+	public static void main(String[] args) {
+		/*Leia 500 valores inteiros e positivos e:
+		 *Encontre o maior.
+		 *Encontre o menor valor
+		 *Calcule a média dos números lidos*/
+		Scanner entrada = new Scanner(System.in);
+		
+		int maior = 0;
+		int menor = 0;
+		int soma = 0;
+		int quantidade = 5;
+		
+		for(int i = 0; i < quantidade; i++) {
+			System.out.println("Digite um número: ");
+			int numero = entrada.nextInt();
+			
+			soma += numero;
+			
+			if (numero > maior) {
+				maior = numero;
+			}
+			//Recebe o primeiro número.
+			if(menor == 0) {
+				menor = numero;
+			}
+			/*Se o número que foi digitado for menor,
+			 *então a variavel menor receber esse número.*/ 
+			if(numero < menor) {
+				menor = numero;
+			}
+			
+
+			
+		}
+	
+		System.out.println("O maior número é: "+maior);
+		System.out.println("O menor número é: "+menor);
+		System.out.println("A média é: "+(soma/quantidade));
+			
+	}	
+}
