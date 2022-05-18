5cf8249857c0190ec165ea2807d882efe54420eb
João Moreira <98332927+Joao-M0@users.noreply.github.com>
2022-05-18 19:16:07 -0300

    Add files via upload

---
+++ Unidade_01/Aula_01/Aula01Exercicio02/src/Programa.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+@SuppressWarnings("all")
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("Digite seu nome completo: ");
+		String nome = new Scanner(System.in).nextLine();
+		System.out.println("Digite sua idade: ");
+		int idade = new Scanner(System.in).nextInt();
+		System.out.println("Você possui algum plano de saúdo? ");
+		String planoSaude = new Scanner(System.in).next();
+		System.out.println("Estado civil");
+		String estadoCivil = new Scanner(System.in).next();
+		
+		
+		System.out.println("\nConfirme os dados preenchidos");
+		
+		System.out.println("Nome: "+nome);
+		System.out.println("Idade: "+idade);
+		System.out.println("Possui plano de saúdo: "+planoSaude);
+		System.out.println("Estado civil: "+estadoCivil);
+		 
+		
+		
+	}
+}
